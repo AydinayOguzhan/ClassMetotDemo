f557bb68638a1db875ac73cce8cb602a229d9e14
Oğuzhan Aydınay <aydinayoguzhan@gmail.com>
2021-01-17 17:47:35 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer(Customer musteri)
+        {
+            Console.WriteLine($"Müşteri eklendi! Eklenen bilgiler aşağıdaki gibidir: \n Id: {musteri.Id} \n İsim: {musteri.FirstName} \n Soy İsim: {musteri.LastName}" +
+                $"\n Tc No: {musteri.TcNumber}");
+        }
+
+        public void DeleteCustomer(int customerId)
+        {
+            Console.WriteLine($"{customerId} id'li kullanıcı silindi.");
+        }
+
+        public void ListCustomer(Customer[] customerList)
+        {
+            foreach (var item in customerList)
+            {
+                Console.WriteLine($"Müşteri Id: {item.Id} \n Müşteri Adı: {item.FirstName} \n Müşteri Soyadı: {item.LastName} \n Müşteri Tc No: {item.TcNumber} " +
+                    $"\n -------------------------------------------------");
+            }
+        }
+
+        public Customer[] CreateCustomerList()
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "Deneme isim 1";
+            customer1.LastName = "Deneme soy isim 1";
+            customer1.TcNumber = "1234567890";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "Deneme isim 2";
+            customer2.LastName = "Deneme soy isim 2";
+            customer2.TcNumber = "0987654321";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Deneme isim 3";
+            customer3.LastName = "Deneme soy isim 3";
+            customer3.TcNumber = "1357902468";
+
+            Customer customer4 = new Customer();
+            customer4.Id = 4;
+            customer4.FirstName = "Deneme isim 4";
+            customer4.LastName = "Deneme soy isim 4";
+            customer4.TcNumber = "2468013579";
+
+            Customer[] customerList = new Customer[] { customer1, customer2, customer3, customer4 };
+
+            return customerList;
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer newCustomer = new Customer();
+            CustomerManager manager = new CustomerManager();
+            bool exitStatement = false;
+
+
+            while (!exitStatement)
+            {
+                Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz: \n 1: Müşteri ekleme \n 2: Müşteri silme \n 3: Müşteri listeleme \n 4: Çıkış");
+                byte answer = Convert.ToByte(Console.ReadLine());
+
+                switch (answer)
+                {
+                    case 1:
+                        Console.WriteLine("Müşteri idsini giriniz: ");
+                        newCustomer.Id = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("Müşteri ismini giriniz: ");
+                        newCustomer.FirstName = Console.ReadLine();
+                        Console.WriteLine("Müşteri soyadını giriniz: ");
+                        newCustomer.LastName = Console.ReadLine();
+                        Console.WriteLine("Müşteri Tc nosunu giriniz: ");
+                        newCustomer.TcNumber = Console.ReadLine();
+
+                        manager.AddCustomer(newCustomer);
+                        break;
+                    case 2:
+                        Console.WriteLine("Lütfen silmek istediğinz müşterinin id sini giriniz: ");
+                        int id = Convert.ToInt16(Console.ReadLine());
+                        manager.DeleteCustomer(id);
+                        break;
+                    case 3:
+                        manager.ListCustomer(manager.CreateCustomerList());
+                        break;
+                    case 4:
+                        exitStatement = true;
+                        break;
+                    default:
+                        Console.WriteLine("Hatalı numara girdiniz!");
+                        break;
+                }
+            }
+        }
+    }
+}
